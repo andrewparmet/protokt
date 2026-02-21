6e8b7d319214bbb3d6f59d305a2407330a963141
Andrew Parmet <andrew@parmet.com>
2026-02-21 12:02:09 -0500

    pure kotlin writer/reader implementation
    Add KotlinCodec, KotlinReader, KotlinWriter, KotlinSinkWriter,
    KotlinSourceReader, and StreamingCodec. Add encodeUtf8Into and
    validateUtf8 to Utf8.kt. Wire up Codec abstraction across
    common/js/jvm with serialize/deserialize dispatch. Add kotlinx.io
    dependency for streaming support.

--- protokt-runtime/src/jsMain/kotlin/protokt/v1/Codec.kt
+++ protokt-runtime/src/jsMain/kotlin/protokt/v1/Codec.kt
@@ -18,4 +18,8 @@
 package protokt.v1
 
 actual val codec: Codec =
-    ProtobufJsCodec
+    if (js("(typeof process !== 'undefined' && process.env && process.env.PROTOKT_CODEC === 'protokt.v1.KotlinCodec')").unsafeCast<Boolean>()) {
+        KotlinCodec
+    } else {
+        ProtobufJsCodec
+    }
